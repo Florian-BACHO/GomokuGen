0f8f2912ff1cb0468bf6c4fd30e1c4189e9331c6
Florian BACHO <florian.bacho@epitech.eu>
2018-10-16 18:16:11 +0200

    Updated trainer - added evaluation

---
+++ ANN.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import game
+
+LEARNING_RATE = 0.01
+
+class ANN:
+    def __init__(self, scope=""):
+        self.scope = scope
+
+        with tf.variable_scope(self.scope):
+            self._create_ann()
+
+    def _create_ann(self):
+        self.x = tf.placeholder(tf.float32, (None, game.BOARD_SIZE, game.BOARD_SIZE, 2))
+        layer1 = tf.layers.conv2d(self.x, 4, 5, padding="same", activation=tf.nn.selu)
+        layer2 = tf.layers.conv2d(layer1, 4, 5, padding="same", activation=tf.nn.selu)
+        layer3 = tf.layers.conv2d(layer2, 8, 3, padding="same", activation=tf.nn.selu)
+        layer4 = tf.layers.conv2d(layer3, 8, 3, padding="same", activation=tf.nn.selu)
+        out = tf.layers.conv2d(layer4, 1, 1, padding="same", activation=tf.nn.selu)
+        self.out_flat = tf.contrib.layers.flatten(out)
+
+        self.expected_y = tf.placeholder(tf.int32, (None))
+        self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.expected_y, logits=self.out_flat))
+        optimizer = tf.train.AdamOptimizer(LEARNING_RATE)
+        self.training_op = optimizer.minimize(self.loss)
+
+    def __call__(self, entries):
+        session = tf.get_default_session()
+
+        return session.run(self.out_flat, feed_dict={self.x: entries})
+
+    def assign(self, otherANN):
+        session = tf.get_default_session()
+        all_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.scope)
+        all_other_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=otherANN.scope)
+        for i, var in enumerate(all_variables):
+            session.run(tf.assign(var, all_other_variables[i]))
+
+    def train(self, entries, expected_outs):
+        session = tf.get_default_session()
+
+        loss, _ = session.run([self.loss, self.training_op], \
+                              feed_dict={self.x: entries, \
+                                         self.expected_y: expected_outs})
+        return loss
--- train.py
+++ train.py
@@ -4,13 +4,15 @@ import tensorflow as tf
 import os
 import random
 import game
+from ANN import *
 
 DATA_DIRS = ["data/2016/freestyle1/", "data/2016/freestyle2/", "data/2016/freestyle3/", \
              "data/2017/freestyle1/", "data/2017/freestyle2/", "data/2017/freestyle3/"]
 LOG_DIR = "logs/"
-LEARNING_RATE = 0.01
 TEST_EVERY_EPOCH = 1000
 DUMP_EPOCH = 100
+NB_FIGHT_EVAL = 100
+UPDATE_THRESHOLD = 0.6
 
 def loadDataFiles(dirs):
     out = []
@@ -67,15 +69,17 @@ def load(file):
 def getRandomFile():
     return random.choice(DATA_FILES)
 
-def test(session, x, y):
+def fight(ann, best, dump=False):
     board = game.getEmptyBoard()
     current = 'O'
     action = random.randint(0, game.BOARD_SIZE * game.BOARD_SIZE - 1)
     board, won = game.move(board, action, current)
-    print(game.render(board))
+    if dump:
+        print(game.render(board))
     current = 'O' if current == 'X' else 'X'
     while True:
-        probs = session.run(y, feed_dict={x: state_lists_to_batch([board], current)})[0]
+        entry = state_lists_to_batch([board], current)
+        probs = best(entry)[0] if current == 'O' else ann(entry)[0]
         while True:
             action = np.argmax(probs)
             if action not in game.possible_moves(board):
@@ -83,27 +87,34 @@ def test(session, x, y):
             else:
                 break
         board, won = game.move(board, action, current)
-        print(game.render(board))
+        if dump:
+            print(game.render(board))
+        if len(game.possible_moves(board)) == 0:
+            return None
         if won:
             break
         current = 'O' if current == 'X' else 'X'
+    return best if current == 'O' else ann
+
+def eval(ann, best):
+    ann_win = 0.0
+    best_win = 0.0
+    for i in range(NB_FIGHT_EVAL):
+        winner = fight(ann, best, dump = (i == 0))
+        if winner == ann:
+            ann_win += 1.0
+        elif winner == best:
+            best_win += 1.0
+    return ann_win / (ann_win + best_win)
 
 if __name__ == "__main__":
-    x = tf.placeholder(tf.float32, (None, game.BOARD_SIZE, game.BOARD_SIZE, 2))
-    layer1 = tf.layers.conv2d(x, 4, 5, padding="same", activation=tf.nn.selu)
-    layer2 = tf.layers.conv2d(layer1, 4, 5, padding="same", activation=tf.nn.selu)
-    layer3 = tf.layers.conv2d(layer2, 8, 3, padding="same", activation=tf.nn.selu)
-    layer4 = tf.layers.conv2d(layer3, 8, 3, padding="same", activation=tf.nn.selu)
-    out = tf.layers.conv2d(layer4, 1, 1, padding="same", activation=tf.nn.selu)
-    out_flat = tf.contrib.layers.flatten(out)
 
-    expected_y = tf.placeholder(tf.int32, (None))
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=expected_y, logits=out_flat)
-    optimizer = tf.train.AdamOptimizer(LEARNING_RATE)
-    training_op = optimizer.minimize(loss)
+    ann = ANN("Main_ANN")
+    best = ANN("Best_ANN")
 
     summary_writer = tf.summary.FileWriter(LOG_DIR)
-    loss_summary = tf.summary.scalar("Loss", tf.reduce_mean(loss))
+    loss_placeholder = tf.placeholder(tf.float32, ())
+    loss_summary = tf.summary.scalar("Loss", loss_placeholder)
 
     init = tf.global_variables_initializer()
 
@@ -118,10 +129,16 @@ if __name__ == "__main__":
             except ValueError:
                 print("Invalid file:", file)
                 continue
-            loss_v, sum, _ = sess.run([loss, loss_summary, training_op], \
-                                 feed_dict={x: entries, expected_y: outs})
+            loss = ann.train(entries, outs)
+            sum = sess.run(loss_summary, feed_dict={loss_placeholder: loss})
             summary_writer.add_summary(sum, epoch)
+
             if epoch % TEST_EVERY_EPOCH == 0:
-                test(sess, x, out_flat)
+                result = eval(ann, best)
+                print("EVALUATION: %f win against the last best ann" % (result))
+                if result >= UPDATE_THRESHOLD:
+                    best.assign(ann)
+                    print("The current ann become the best one")
+
             if epoch % DUMP_EPOCH == 0:
-                print("%d: Loss: %f" % (epoch, loss_v.mean()))
+                print("%d: Loss: %f" % (epoch, loss))
